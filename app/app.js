ab35a95bf6ef9390cdcbe9459aefb82b43523d34
Oliver Kulpakko <oliver@kulpakko.com>
2019-06-27 13:40:28 +0200

    Small formatting changes.

--- app/app.js
+++ app/app.js
@@ -1,20 +1,20 @@
 fetchLayers(function(layers, error) {
   if (error) {
     alert(error);
-    return
+    return;
   }
 
   setupMap(layers);
 });
 
 function setupMap(layers) {
-  let map = L.map('map').fitWorld();
+  let map = L.map("map").fitWorld();
 
   L.control.layers(layers).addTo(map);
   L.control.locate().addTo(map);
-  
-  map.on('locationfound', onLocationFound);
-  map.on('locationerror', onLocationError);
+
+  map.on("locationfound", onLocationFound);
+  map.on("locationerror", onLocationError);
 
   map.locate({
     setView: true,
@@ -40,7 +40,7 @@ function onLocationError(e) {
 // Data
 
 function fetchLayers(callback) {
-  let url = 'https://kulpakko.com/api/topomaps/maps';
+  let url = "https://kulpakko.com/api/topomaps/maps";
 
   fetch(url)
     .then(res => res.json())
@@ -51,13 +51,13 @@ function fetchLayers(callback) {
         let urls = [];
         for (url of map.urls) {
           urls.push(L.tileLayer(url.template, {
-            attribution: '<a href="' + map.copyright.url + '">' + map.copyright.text + '</a>',
+            attribution: "<a href=\"" + map.copyright.url + "\">" + map.copyright.text + "</a>",
             minZoom: url.minZoom,
             maxZoom: url.maxZoom
           }));
         }
 
-        let name = map.country + ' - ' + map.type;
+        let name = map.country + " - " + map.type;
 
         layers[name] = L.layerGroup(urls);
       }
