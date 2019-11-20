8b2c0e642e36723b6a8174a7bcf5480a55f125c8
Oliver Kulpakko <oliver@kulpakko.com>
2019-11-20 18:45:17 +0200

    Design changes to home.

---
+++ all-maps/js/all-maps.js
@@ -0,0 +1,63 @@
+fetch('https://api.topomaps.app/maps')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        let countries = Object.create(null);
+
+        data.forEach(element => {
+            if (countries[element.country] == null) {
+                countries[element.country] = [element];
+            } else {
+                countries[element.country].push(element);
+            }
+        });
+
+        var cardsContainer = document.getElementById('cards');
+
+        Object.keys(countries).forEach(function(country, index) {
+            let card = document.createElement('div');
+            card.className = 'card mb-3';
+
+            let cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            let countryTitle = document.createElement('h3');
+            countryTitle.className = 'card-title';
+            countryTitle.textContent = getCountryName(country);
+            cardBody.appendChild(countryTitle);
+
+            let itemsList = document.createElement('ul');
+
+            countries[country].forEach(element => {
+                let itemElement = document.createElement('li');
+                itemElement.textContent = formattedName(element.type) + ' (' + element.copyright + ')';
+
+                itemsList.appendChild(itemElement);
+            });
+
+            cardBody.appendChild(itemsList);
+
+            card.appendChild(cardBody);
+
+            cardsContainer.appendChild(card);
+        })
+    })
+    .catch(err => {
+        alert(err);
+    })
+
+function formattedName(mapType) {
+    switch (mapType) {
+        case 'topo':
+            return "Topo";
+        case 'imagery':
+            return "Imagery";
+        case 'nautical':
+            return "Nautical Charts";
+        case 'imagery_topo':
+            return "Imagery + Topo";
+        default:
+            return mapType;
+    }
+}
